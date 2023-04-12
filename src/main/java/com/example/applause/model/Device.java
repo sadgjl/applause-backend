b3a19fe8539e6ebf8a5a54abbe0026e7e048721d
sadgjl <57816858+sadgjl@users.noreply.github.com>
2023-04-12 10:32:25 -0400

    migration to Spring boot 3

--- src/main/java/com/example/applause/model/Device.java
+++ src/main/java/com/example/applause/model/Device.java
@@ -1,11 +1,11 @@
 package com.example.applause.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "devices")
--- src/main/java/com/example/applause/model/Tester.java
+++ src/main/java/com/example/applause/model/Tester.java
@@ -1,8 +1,8 @@
 package com.example.applause.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
-import javax.persistence.*;
 import java.util.Date;
 
 @Entity
